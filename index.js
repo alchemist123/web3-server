da3445db3505a7c5d0283e3fc4542bd41c48baf4
amal vs <amalvs@metrictreelabs.com>
2023-04-03 18:30:55 +0530

    yml changed

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const port = 5050
 const Gun = require('gun')
+require('gun/axe');
 
 app.use(Gun.serve)
+app.use(express.static('/gun'));
 const server = app.listen(port, () => {
   console.log(`Gun server running on port ${port}🔥`)
 })
 
-Gun({file: 'data', web: server })
+Gun({web: server, file: 'testingDB'});

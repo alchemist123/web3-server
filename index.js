41cf537eb26d8fdb9a156c7c045c310cf5e444ff
amal vs <amalvs@metrictreelabs.com>
2023-04-04 09:39:00 +0530

    mmmmm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5050
+const port = 5051
 const Gun = require('gun')
 require('gun/axe');
 var fs = require('fs');
@@ -10,11 +10,4 @@ app.use(express.static('/gun'));
 const server = app.listen(port, () => {
   console.log(`Gun server running on port ${port}🔥`)
 })
-fs.access('./testingDB', fs.constants.X_OK, (err) => {
-  if (err) {
-  console.log("%s doesn't exist");
- } else {
-  Gun({web: server, file: "./testingDB"});
-  console.log('can execute %s');
-  }
- });
+Gun({web: server, file: "./testingDb.json"});

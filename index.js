a739a4b0de18bcf8e9958192b4c80d985ea3d691
amal vs <amalvs@metrictreelabs.com>
2023-04-04 09:59:33 +0530

    made change

--- index.js
+++ index.js
@@ -3,18 +3,15 @@ const app = express()
 const port = 5051
 const Gun = require('gun')
 require('gun/axe');
-var fs = require('fs');
-var chmodr = require('chmodr');
-var path='./testingDB'
 app.use(Gun.serve)
-const server = app.listen(port, () => {
-  console.log(`Gun server running on port ${port}🔥`)
+const server = require('http').createServer(Gun.serve(__dirname));
+
+// Pass the validation function as isValid
+const gun = Gun({
+  file: 'data.json',
+  web: server,
 })
-chmodr('./testingDB', 777, (err) => {
-  if (err) {
-    console.log('Failed to execute chmod', err);
-  } else {
-    Gun({web: server, file: "./testingDB"});
-    console.log('Success');
-  }
-});
+
+// Sync everything
+gun.on('out', {get: {'#': {'*': ''}}})
+server.listen(8000)

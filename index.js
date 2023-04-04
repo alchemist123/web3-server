8af222760b64682e6ed1de32d5bcc2cb03b2d59a
amal vs <amalvs@metrictreelabs.com>
2023-04-04 16:38:44 +0530

    allset

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // in the index.js file
-
+var CronJob = require('cron').CronJob;
 const express = require('express')
 const app = express()
 const port = 5050
 const Gun = require('gun')
+const fs = require('fs')
+var cronJob1 = new CronJob({
 
+  cronTime: '0 0 0 * * *',
+  onTick: function () {
+    fs.rmSync('./radata', { recursive: true });
+    console.log("done fs")
+  },
+  start: true,
+  runOnInit: false
+});
 app.use(Gun.serve)
 
 const server = require('http').createServer().listen(5050);

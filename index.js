a227f1a094bacdc5b7d242256d6fac5691b6567f
amal vs <amalvs@metrictreelabs.com>
2023-04-04 09:49:29 +0530

    permissions 777

--- index.js
+++ index.js
@@ -10,26 +10,8 @@ app.use(Gun.serve)
 const server = app.listen(port, () => {
   console.log(`Gun server running on port ${port}🔥`)
 })
-chmodr('./testingDB', 0o777, (err) => {
+chmodr('./testingDB', 777, (err) => {
   if (err) {
-    fs.access(path, (error) => {
-   
-      // To check if the given directory 
-      // already exists or not
-      if (error) {
-        // If current directory does not exist
-        // then create it
-        fs.mkdir(path, (error) => {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log("New Directory created successfully !!");
-          }
-        });
-      } else {
-        console.log("Given Directory already exists !!");
-      }
-    });
     console.log('Failed to execute chmod', err);
   } else {
     Gun({web: server, file: "./testingDB"});

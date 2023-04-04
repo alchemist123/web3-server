fcaefeec48c9fb190824d1c32202ab1f80b01d8d
amal vs <amalvs@metrictreelabs.com>
2023-04-04 10:10:19 +0530

    made changes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const port = 5051
 const Gun = require('gun')
-require('gun/axe');
 app.use(Gun.serve)
 const server = require('http').createServer(Gun.serve(__dirname));
 
@@ -10,6 +9,10 @@ const server = require('http').createServer(Gun.serve(__dirname));
 const gun = Gun({
   file: 'data.json',
   web: server,
+  localStorage: true,
+  radisk: true,
+  axe: true
+
 })
 
 // Sync everything
